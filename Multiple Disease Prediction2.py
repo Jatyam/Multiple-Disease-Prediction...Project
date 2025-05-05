03c22d4a8a32e49aae13bf048978848bbe935cb5
Jatyam Billawria <jatyambillawria4151@outlook.com>
2025-05-05 08:17:35 +0530

    Update Multiple Disease Prediction2.py

--- Multiple Disease Prediction2.py
+++ Multiple Disease Prediction2.py
@@ -19,11 +19,11 @@ st.set_page_config(page_title="Health Assistant",
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # loading the saved models
-diabetes_model = pickle.load(open('C:/Users/sohan/Downloads/Multiple Disease Prediction...Project/Saved Models/diabetes_model.sav','rb'))
-heart_disease_model = pickle.load(open('C:/Users/sohan/Downloads/Multiple Disease Prediction...Project/Saved Models/heart_disease_model.sav','rb'))
-parkinsons_model = pickle.load(open('C:/Users/sohan/Downloads/Multiple Disease Prediction...Project/Saved Models/parkinsons_model.sav','rb'))
-lung_cancer_model = pickle.load(open('C:/Users/sohan/Downloads/Multiple Disease Prediction...Project/Saved Models/lung_cancer_model.sav','rb'))
-
+diabetes_model = pickle.load(open('Saved Models/diabetes_model.sav','rb'))
+heart_disease_model = pickle.load(open('Saved Models/heart_disease_model.sav','rb'))
+parkinsons_model = pickle.load(open('Saved Models/parkinsons_model.sav','rb'))
+lung_cancer_model = pickle.load(open('Saved Models/lung_cancer_model.sav','rb'))
+#C:/Users/sohan/Downloads/Multiple Disease Prediction...Project/
 # Sidebar for navigation
 with st.sidebar:
     selected = option_menu('Multiple Desease Prediction System',
